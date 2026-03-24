23f27b509baaa0e497b78f9caf37216324376adc
Walmir Junior <junioroliveira8100@gmail.com>
2026-03-24 20:58:37 -0300

    Adicionada pontuacao do jogador

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -20,10 +20,24 @@ using System.Security.Cryptography;
             Fácil (intervalo 1 a 20): ≈ 10 tentativas.
             Médio (intervalo 1 a 50): ≈ 5 tentativas.
          Difícil (intervalo 1 a 100): ≈ 3 tentativas.
+
+        2. Implemente uma funcionalidade de Validação de Números Repetidos
+
+            O jogador deve ser informado caso o número que está tentando adivinhar já tenha sido informado anteriormente na mesma rodada.
+        3. Implemente uma funcionalidade de Pontuação, onde:
+
+            O jogador começa com uma pontuação máxima, por exemplo, 1000 pontos.
+            A pontuação é calculada com base na proximidade do palpite em relação ao número secreto.
+
+            A cada tentativa errada, o jogador perde pontos de acordo com a distância do número secreto:
+                Se a diferença entre o número secreto e o palpite for de 10 ou mais, o jogador perde 100 pontos.
+                Se a diferença for entre 5 e 9, o jogador perde 50 pontos.
+                Se a diferença for entre 1 e 4, o jogador perde 20 pontos.
+
+        4. nosso jogo deve permitir múltiplas tentativas ao usúario
 */
 
-// 4. nosso jogo deve permitir múltiplas tentativas ao usúario
-
+//array
 
 bool jogoDeveContinuar = true;
 
@@ -70,6 +84,11 @@ while (jogoDeveContinuar == true) //false
             continue;
     }
 
+    int pontuacao = 1000;
+
+    int[] numerosDigitados = new int[tentativasMaximas];
+    int contadorNumerosDigitados = 0;
+
     // enquanto a tentativa atual for menor que a qtd de tentativas maximas
     for (int tentativaAtual = 1; tentativaAtual <= tentativasMaximas; tentativaAtual++)
     {
@@ -81,6 +100,45 @@ while (jogoDeveContinuar == true) //false
         Console.WriteLine("-------------------------------------");
         Console.Write("Digite um número: ");
         int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+        //comparar com outros numeros na memória
+        bool numeroEstaRepetido = false;
+        for (int indiceAtual = 0; indiceAtual < numerosDigitados.Length; indiceAtual++)
+        {
+            if (numerosDigitados[indiceAtual] == numeroDigitado)
+            {
+                numeroEstaRepetido = true;
+                break;
+            }
+        }
+
+        if (numeroEstaRepetido == true)
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Voce ja digitou este número, tente novamente.");
+            Console.WriteLine("-------------------------------------");
+
+            Console.Write("Digite ENTER para continuar...");
+            Console.ReadLine();
+
+            tentativaAtual--;
+            continue;
+        }
+
+        //guardar o número na memória
+        if (contadorNumerosDigitados < numerosDigitados.Length)
+        {
+            numerosDigitados[contadorNumerosDigitados] = numeroDigitado;
+            contadorNumerosDigitados++;
+        }
+        else
+        {
+            numerosDigitados = new int[tentativasMaximas];
+            contadorNumerosDigitados = 0;
+
+            numerosDigitados[contadorNumerosDigitados] = numeroDigitado;
+            contadorNumerosDigitados++;
+        }
 
         if (numeroDigitado == numeroAleatorio)
         {
@@ -98,10 +156,26 @@ while (jogoDeveContinuar == true) //false
             Console.WriteLine("O número digitado foi menor que o número secreto!");
         }
 
-        Console.WriteLine("-------------------------------------");
-        Console.Write("Digite ENTER para continuar...");
-        Console.ReadLine();
+        int diferencaNumerica = Math.Abs(numeroAleatorio - numeroDigitado); // 90 - 100 = -10
 
+        if (diferencaNumerica >= 10)
+        {
+            pontuacao -= 100;
+        }
+        else if (diferencaNumerica >= 5)
+        {
+            pontuacao -= 50;
+        }
+        else
+        {
+            pontuacao -= 20;
+        }
+
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("sua pontuaçao é: " + pontuacao);
+        Console.Write("Digite ENTER para continuar...");
+        Console.WriteLine("-------------------------------------");
+        Console.ReadLine();
     }
 
     Console.Write("Deseja continuar? (s/N): ");
